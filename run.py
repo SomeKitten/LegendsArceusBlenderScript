8ab17ffb15f7fa9072c6099ada96bcb1d0721a31
Teras <teras@amonia.de>
2022-12-29 23:18:52 +0100

    cleanup
    remove unnecessary files

--- run.py
+++
@@ -1,8 +0,0 @@
-from PokemonSwitch import from_trmdl
-#from tmp import from_trmdl
-
-#f = open("F:\\Datamining\\PokemonScarlet\\extracted\\field_graphic\\common_model\\a_sch_entrance01\\a_sch_entrance01.trmdl", "rb")
-#from_trmdl("F:\\Datamining\\PokemonScarlet\\extracted\\field_graphic\\common_model\\a_sch_entrance01",f,False,False,False)
-
-f = open("F:\\Datamining\\PokemonScarlet\\extracted\\field_graphic\\common_model\\a_sch_entrance01\\a_sch_entrance01.trmdl", "rb")
-from_trmdl("F:\\Datamining\\PokemonScarlet\\extracted\\field_graphic\\common_model\\a_sch_entrance01",f,False,False,False)
